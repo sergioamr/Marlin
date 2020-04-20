941322707bac47e57c3bd85f63512a2d34239e0c
Sergio A. Mart <sergio@triditive.com>
2020-04-20 12:29:12 +0200

    [M32] Allow to run the M32 from inside another gcode
    Signed-off-by: Sergio A. Mart <sergio@triditive.com>

--- Marlin/cardreader.h
+++ Marlin/cardreader.h
@@ -152,7 +152,7 @@ private:
   SdVolume volume;
   SdFile file;
 
-  #define SD_PROCEDURE_DEPTH 1
+  #define SD_PROCEDURE_DEPTH 3
   #define MAXPATHNAMELENGTH (FILENAME_LENGTH*MAX_DIR_DEPTH + MAX_DIR_DEPTH + 1)
   uint8_t file_subcall_ctr;
   uint32_t filespos[SD_PROCEDURE_DEPTH];
